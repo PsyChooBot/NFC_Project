a1085a47d4f9ae997a9ccc89437a4e399fa589ed
Valentina <https://github.com/PsyChooBot/NFC_Project>
2021-12-25 00:40:06 +0100

    Ho modificato il SettingsActivity.java + activity_settings.xml e intent di setting nel MainActivity.java

--- app/src/main/java/com/example/alpha_project/MainActivity.java
+++ app/src/main/java/com/example/alpha_project/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.alpha_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private  Button settings;
@@ -35,12 +34,34 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-public void openSettings() {
-        Intent intent = new Intent(this,SettingsActivity.class);
-        startActivity(intent);
-}
-public void openShare(){
+
+        public void openSettings() {
+         Intent setting = new Intent(this, SettingsActivity.class);
+            startActivity(setting);
+    }
+
+        // This method is called when the second activity finishes
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            /* check that it is the SecondActivity with an OK result */
+            if (requestCode == 1) {
+                if (resultCode == RESULT_OK) {
+                    // get String data from Intent
+                    String nome = getIntent().getStringExtra("name");
+                    String cognome = getIntent().getStringExtra("surname");
+                    String phone = getIntent().getStringExtra("Phone Number");
+                    String mail = getIntent().getStringExtra("E-mail");
+                    String instagram = getIntent().getStringExtra("Instagram Tag");
+                    String linkedin = getIntent().getStringExtra("Linkedin");
+                }
+            }
+        }
+
+    public void openShare(){
         Intent intent = new Intent(this,ShareActivity.class);
         startActivity(intent);
+    }
 }
-}
+
--- app/src/main/java/com/example/alpha_project/SettingsActivity.java
+++ app/src/main/java/com/example/alpha_project/SettingsActivity.java
@@ -2,13 +2,41 @@ package com.example.alpha_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+    public void Save_Return(View view) {
+
+        EditText name = (EditText) findViewById(R.id.nome);
+        EditText surname = (EditText) findViewById(R.id.cognome);
+        EditText phonenumber = (EditText) findViewById(R.id.phone);
+        EditText email = (EditText) findViewById(R.id.mail);
+        EditText instagram= (EditText) findViewById(R.id.instagram);
+        EditText linkedin = (EditText) findViewById(R.id.linkedin);
+
+
+        Bundle data = new Bundle();
+        data.putString("name", name.getText().toString());
+        data.putString("Surname", surname.getText().toString());
+        data.putString("Phone Number", phonenumber.getText().toString());
+        data.putString("E-mail", email.getText().toString());
+        data.putString("Instagram Tag", instagram.getText().toString());
+        data.putString("Linkedin", linkedin.getText().toString());
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_TEXT, data);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+
 }
