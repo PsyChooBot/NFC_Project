412e58a1d5e7b54d547347d2e00a0dda85ce2f2a
1007tina <96027230+1007tina@users.noreply.github.com>
2021-12-25 23:19:18 +0100

    Update SettingsActivity.java

--- app/src/main/java/com/example/alpha_project/SettingsActivity.java
+++ app/src/main/java/com/example/alpha_project/SettingsActivity.java
@@ -22,7 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
 
     protected EditText etname, etphon,etmail,etsurname, etins, etlinke;
     public File f;
-    private SharedPreferences mPrefs;
+    private SharedPreferences mPrefs; // la mia preferenza per utilizzare onPause();
     private String name;
     private String surname;
     private String phon;
@@ -42,7 +42,8 @@ public class SettingsActivity extends AppCompatActivity {
         etins= findViewById(R.id.instagram);
         etlinke= findViewById(R.id.linkedin);
 
-        // Imposto i valori dell'EditText con i valori memorizzati
+        /* Imposto i valori dell'EditText con i valori memorizzati : per esempio quando esco dall'activity setting e ritorno al main avendo richiamato nel codice onPause salvo 
+        le varie informazioni e se non avevo scritto niente di default salvo null*/
         mPrefs = getSharedPreferences(getLocalClassName(), MODE_PRIVATE);
         name = mPrefs.getString("Name",null);
         etname.setText(name);
@@ -57,6 +58,8 @@ public class SettingsActivity extends AppCompatActivity {
         linke = mPrefs.getString("LinkedinTag",null);
         etlinke.setText(linke);
 
+        
+        // è il tasto per creare un file vcf
         Button create = findViewById(R.id.create);
 
         create.setOnClickListener(new View.OnClickListener() {
@@ -94,12 +97,12 @@ public class SettingsActivity extends AppCompatActivity {
         /* Button that save information and create an vcard file vcf */
 
     public void openSave() throws IOException {
-
+        // creo un file con path contact.vcf
         f=new File("contact.vcf");
         FileOutputStream fop=new FileOutputStream(f);
 
         if(f.exists())
-        {
+        {   //inizio a scrivere nella stringa str ciò che voglio inserire nel file vcf
             String str="BEGIN:VCARD\n" +
                     "VERSION:4.0\n" +
                     "N:"+etname.getText().toString()+" " + etsurname.getText().toString()+";;;\n" +
@@ -109,7 +112,7 @@ public class SettingsActivity extends AppCompatActivity {
                     "INSTAGRAM:"+etins.getText().toString()+"\n"+
                     "LINKEDIN:"+etlinke.getText().toString()+"\n"+
                     "END:VCARD";
-            fop.write(str.getBytes());
+            fop.write(str.getBytes()); // inserisco la stringa dentro il file
             //Now read the content of the vCard after writing data into it
             BufferedReader br = null;
             String sCurrentLine;
