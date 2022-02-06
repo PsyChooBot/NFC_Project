c238e3a8cd10c1b9e62cab83c10074d691206dae
Valentina <https://github.com/PsyChooBot/NFC_Project>
2022-02-06 13:05:51 +0100

    Nel SettingsActivity.java usato lo sharepreferences per salvare i dati ma ancora ho il problema che non mi salva il dato che inserisco per il Name , se salvo ciò che scrivo per Surname me lo salva automaticamente anche per Name ma guardando il codice non vedo il problema. Ho inoltre due button uno per reset e uno per return alla activity principale.

--- app/src/main/java/com/example/alpha_project/SettingsActivity.java
+++ app/src/main/java/com/example/alpha_project/SettingsActivity.java
@@ -2,34 +2,28 @@ package com.example.alpha_project;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.annotation.RequiresApi;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+
 
 
 public class SettingsActivity extends AppCompatActivity {
 
     protected EditText etname, etphon,etmail,etsurname, etins, etlinke;
     public File f;
-    private SharedPreferences mPrefs; // la mia preferenza per utilizzare onPause();
-    private String name;
-    private String surname;
-    private String phon;
-    private String mail;
-    private String ins;
-    private String linke;
-
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String Name = "nameKey";
+    public static final String Surname = "nameKey";
+    public static final String Phone = "phoneKey";
+    public static final String Email = "emailKey";
+    public static final String Ins = "insKey";
+    public static final String Linke = "linkeKey";
+    SharedPreferences sharedpreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,37 +37,49 @@ public class SettingsActivity extends AppCompatActivity {
         etlinke= findViewById(R.id.linkedin);
 
         /* Imposto i valori dell'EditText con i valori memorizzati : per esempio quando esco dall'activity setting e ritorno al main avendo richiamato nel codice onPause salvo 
-        le varie informazioni e se non avevo scritto niente di default salvo null*/
-        mPrefs = getSharedPreferences(getLocalClassName(), MODE_PRIVATE);
-        name = mPrefs.getString("Name",null);
-        etname.setText(name);
-        surname = mPrefs.getString("Surname",null);
-        etsurname.setText(surname);
-        phon = mPrefs.getString("Phone Number",null);
-        etphon.setText(phon);
-        mail = mPrefs.getString("E-Mail",null);
-        etmail.setText(mail);
-        ins = mPrefs.getString("Instagram Tag",null);
-        etins.setText(ins);
-        linke = mPrefs.getString("LinkedinTag",null);
-        etlinke.setText(linke);
+        le varie informazioni e se non avevo scritto niente di default salvo il valore di default*/
+        sharedpreferences = getSharedPreferences(MyPREFERENCES,MODE_PRIVATE);
+        String n = sharedpreferences.getString(Name,null);
+        String su = sharedpreferences.getString(Surname,null);
+        String ph = sharedpreferences.getString(Phone,null);
+        String e = sharedpreferences.getString(Email,null);
+        String i = sharedpreferences.getString(Ins,null);
+        String l = sharedpreferences.getString(Linke,null);
 
-        
-        // è il tasto per creare un file vcf
-        Button create = findViewById(R.id.create);
+        etname.setText(n);
+        etsurname.setText(su);
+        etphon.setText(ph);
+        etmail.setText(e);
+        etins.setText(i);
+        etlinke.setText(l);
 
-        create.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
+        Button b1 = (Button) findViewById(R.id.save);
+        // è il tasto per salvare i dati
+
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    openSave();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+                String n = etname.getText().toString();
+                String su = etsurname.getText().toString();
+                String ph = etphon.getText().toString();
+                String e = etmail.getText().toString();
+                String i = etins.getText().toString();
+                String l = etlinke.getText().toString();
 
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString(Name, n);
+                editor.putString(Surname, su);
+                editor.putString(Phone, ph);
+                editor.putString(Email, e);
+                editor.putString(Ins, i);
+                editor.putString(Linke, l);
+
+                editor.apply();
+                Toast.makeText(SettingsActivity.this,"Saved",Toast.LENGTH_LONG).show();
+            }
         });
+
+
         Button reset = (Button) findViewById(R.id.RESETBTN);
 
         reset.setOnClickListener(new View.OnClickListener() {
@@ -94,40 +100,6 @@ public class SettingsActivity extends AppCompatActivity {
 
         });
     }
-        /* Button that save information and create an vcard file vcf */
-
-    public void openSave() throws IOException {
-        // creo un file con path contact.vcf
-        f=new File("contact.vcf");
-        FileOutputStream fop=new FileOutputStream(f);
-
-        if(f.exists())
-        {   //inizio a scrivere nella stringa str ciò che voglio inserire nel file vcf
-            String str="BEGIN:VCARD\n" +
-                    "VERSION:4.0\n" +
-                    "N:"+etname.getText().toString()+" " + etsurname.getText().toString()+";;;\n" +
-                    "FN: " +etsurname.getText().toString()+" " + etname.getText().toString()+"\n"+
-                    "PHON;TYPE=work,voice;VALUE=uri:tel:"+etphon.getText().toString()+"\n"+
-                    "EMAIL:"+etmail.getText().toString()+"\n"+
-                    "INSTAGRAM:"+etins.getText().toString()+"\n"+
-                    "LINKEDIN:"+etlinke.getText().toString()+"\n"+
-                    "END:VCARD";
-            fop.write(str.getBytes()); // inserisco la stringa dentro il file
-            //Now read the content of the vCard after writing data into it
-            BufferedReader br = null;
-            String sCurrentLine;
-            br = new BufferedReader(new FileReader("contact.vcf"));
-            while ((sCurrentLine = br.readLine()) != null)
-            {
-                System.out.println(sCurrentLine);
-            }
-            //close the output stream and buffer reader
-            fop.flush();
-            fop.close();
-            System.out.println("The data has been written");
-        } else
-            System.out.println("This file does not exist");
-    }
 
     /*  Metodo reset button*/
   public void openReset(){
@@ -137,6 +109,7 @@ public class SettingsActivity extends AppCompatActivity {
       etins.setText(null);
       etmail.setText(null);
       etlinke.setText(null);
+      Toast.makeText(SettingsActivity.this,"Reseted",Toast.LENGTH_LONG).show();
     }
 
     /*  Metodo return button --- ritorno a MainActivity*/
@@ -144,25 +117,5 @@ public class SettingsActivity extends AppCompatActivity {
        finish();
 
     }
-
-    // Anche se esco da SettingsActivity non perdo le informazioni inserite
-    protected void onPause() {
-        super.onPause();
-
-        // Salvo i valori nelle preferenze
-        SharedPreferences.Editor name = mPrefs.edit().putString("Name", etname.getText().toString());
-        name.apply();
-        SharedPreferences.Editor surname = mPrefs.edit().putString("Surname", etsurname.getText().toString());
-        surname.apply();
-        SharedPreferences.Editor phone = mPrefs.edit().putString("Phone Number", etphon.getText().toString());
-        phone.apply();
-        SharedPreferences.Editor e_mail = mPrefs.edit().putString("E-Mail", etmail.getText().toString());
-        e_mail.apply();
-        SharedPreferences.Editor instagram = mPrefs.edit().putString("Instagram Tag", etins.getText().toString());
-        instagram.apply();
-        SharedPreferences.Editor linkedin = mPrefs.edit().putString("LinkedinTag", etlinke.getText().toString());
-        linkedin.apply();
-    }
-
 
 }
